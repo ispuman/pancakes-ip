c9d0b15b1d4ca0cfe387667db4bb912115abcdb9
iskrensp <ispuman@gmail.com>
2025-09-05 00:03:42 +0100

    improve pancake error messages

--- src/main/java/org/pancakelab/factory/PancakeFactoryImpl.java
+++ src/main/java/org/pancakelab/factory/PancakeFactoryImpl.java
@@ -5,6 +5,12 @@ import org.pancakelab.model.pancake.*;
 import java.util.List;
 
 public class PancakeFactoryImpl implements PancakeFactory {
+
+    private static final List<String> menu = new SaltyPancake().getMenu().stream()
+            .map(p -> p.replaceFirst("Pancake", "").toLowerCase()).toList();
+
+    public static final String INVALID_PANCAKE_TYPE = "Please select valid pancake type : %s".formatted(menu);
+
     @Override
     public Pancake createPancake(String type) {
         PancakeRecipe pancake = switch (type) {
@@ -12,14 +18,14 @@ public class PancakeFactoryImpl implements PancakeFactory {
             case "sweet" -> new SweetPancake();
             case "vegetarian" -> new VegetarianPancake();
             case "meat" -> new MeatPancake();
-            default -> throw new IllegalStateException("Unexpected value for pancake type : " + type);
+            default -> throw new IllegalArgumentException(INVALID_PANCAKE_TYPE);
         };
         return new Pancake(pancake);
     }
 
     @Override
     public List<String> getMenu() {
-        return new SaltyPancake().getMenu();
+        return menu;
     }
 
     @Override
--- src/test/java/org/pancakelab/model/pancake/PancakeTest.java
+++ src/test/java/org/pancakelab/model/pancake/PancakeTest.java
@@ -2,16 +2,34 @@ package org.pancakelab.model.pancake;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.pancakelab.dto.PancakeDTO;
 import org.pancakelab.factory.PancakeFactory;
 import org.pancakelab.factory.PancakeFactoryImpl;
+import org.pancakelab.model.client.Disciple;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.pancakelab.factory.PancakeFactoryImpl.INVALID_PANCAKE_TYPE;
 import static org.pancakelab.model.pancake.Pancake.PANCAKE_INGREDIENT_TYPO;
 import static org.pancakelab.model.pancake.Pancake.UNAVAILABLE_INGREDIENT_FOR_PANCAKE_TYPE;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PancakeTest {
+
+    @Test
+    public void GivenOrderingPancake_WhenInvalidTypeIsRequested_TheAppropriateErrorMessageIsDisplayed() {
+        // setup
+        Disciple disciple = new Disciple("Tom", 2, 3);
+        disciple.createOrder(2, 3);
+        // exercise
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> disciple.addPancake(new PancakeDTO("sweat", List.of("honey", "bananas"))));
+        // verify
+        assertEquals(INVALID_PANCAKE_TYPE, ex.getMessage());
+        // tear down
+    }
 
     @Test
     public void GivenOrderingPancake_WhenAddingInvalidIngredient_TheAppropriateErrorMessageIsDisplayed() {
@@ -22,7 +40,7 @@ public class PancakeTest {
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> pancake.addIngredient("hazelnut"));
         // verify
         assertEquals(PANCAKE_INGREDIENT_TYPO.formatted(Ingredient.VALUES), ex.getMessage());
-        // verify
+        // tear down
     }
 
     @Test
